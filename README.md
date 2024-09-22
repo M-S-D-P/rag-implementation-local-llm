8ec35d973de655f61da6c816afce66f66d1a03d4
M <ms@Mallelas-MacBook-Pro.local>
2024-09-22 21:59:55 +0530

    rag with local llm and vector database integration

---
+++ README.md
@@ -0,0 +1,9 @@
+# rag-with-local-llm
+
+## Key components
+- Rag Implementation with Vector Database
+- Generating embeddings
+- Chunking
+- Data extraction from files & storing
+- LLAM2 setup and integration
+- Prompts to get answers using data stored with LLAM2
---
+++ get_embedding_function.py
@@ -0,0 +1,8 @@
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_community.embeddings.bedrock import BedrockEmbeddings
+
+
+def get_embedding_function():
+    
+    embeddings = OllamaEmbeddings(model="llama2")
+    return embeddings
